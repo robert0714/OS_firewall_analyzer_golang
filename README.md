7f6603a664adc9484c2423a6cf1f5ad055213510
robert0714 <robert0714@gmail.com>
2025-03-15 14:00:15 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Using Visual Code to debug 
+* step 1
+```
+go install -v github.com/haya14busa/goplay/cmd/goplay@v1.0.0
+```
+* step 2 add `.vscode/launch.json`
+```json
+{
+  "version": "0.2.0",
+  "configurations": [
+    {
+      "name": "Launch",
+      "type": "go",
+      "request": "launch",
+      "mode": "debug",
+      "program": "${workspaceFolder}"
+    }
+  ]
+}
+
+```
+* step 3
+  * type `Ctrl + Shift + D`
+  * press button `Launch`
+  * press button `F5`
---
+++ firewall_rules_converter.go
@@ -0,0 +1,347 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+type Rule struct {
+	RuleID          string `json:"rule_id"`
+	RuleName        string `json:"rule_name"`
+	RuleDisplayName string `json:"rule_display_name"`
+	Chain           string `json:"chain"`
+	Table           string `json:"table"`
+	Protocol        string `json:"protocol"`
+	SrcAddress      string `json:"src_address"`
+	DstAddress      string `json:"dst_address"`
+	SrcPort         string `json:"src_port"`
+	DstPort         string `json:"dst_port"`
+	Action          string `json:"action"`
+}
+type WinRule struct {
+	RuleID          int         `json:"rule_id"`
+	RuleName        string      `json:"Name"`
+	RuleDisplayName string      `json:"DisplayName"`
+	Description     string      `json:"Description"`
+	Direction       int         `json:"Direction"`
+	Action          int         `json:"Action"`
+	Protocol        string      `json:"Protocol"`
+	LocalPort       string      `json:"LocalPort"`
+	RemoteAddress   string      `json:"RemoteAddress"`
+	LocalAddress    interface{} `json:"LocalAddress"` // 可能是 string 或 object
+}
+
+type AlgoSecData struct {
+	Version        string                 `json:"version"`
+	ConfigType     string                 `json:"config_type"`
+	Device         map[string]interface{} `json:"device"`
+	Hosts          map[string]interface{} `json:"hosts"`
+	HostsGroups    map[string]interface{} `json:"hosts_groups"`
+	Services       map[string]interface{} `json:"services"`
+	ServicesGroups map[string]interface{} `json:"services_groups"`
+	Policies       map[string]interface{} `json:"policies"`
+}
+
+// 解析 LocalAddress，確保支援不同格式
+func parseLocalAddress(raw interface{}) string {
+	switch v := raw.(type) {
+	case string:
+		return v
+	case map[string]interface{}:
+		if val, ok := v["value"].([]interface{}); ok {
+			// 轉換成字串陣列
+			var addresses []string
+			for _, addr := range val {
+				addresses = append(addresses, fmt.Sprintf("%v", addr))
+			}
+			return fmt.Sprintf("[%s]", strconv.Quote(fmt.Sprint(addresses)))
+		}
+	}
+	return ""
+}
+
+func getWindowsFirewallRules() ([]Rule, error) {
+	psScript := "get_windows_firewall_rules.ps1"
+	cmd := exec.Command("powershell", "-ExecutionPolicy", "Bypass", "-File", psScript)
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, fmt.Errorf("error getting Windows firewall rules: %v", err)
+	}
+
+	var rules []Rule
+	if err := json.Unmarshal(output, &rules); err != nil {
+		return nil, fmt.Errorf("error parsing Windows firewall rules: %v", err)
+	}
+
+	return rules, nil
+}
+func getWindowsFirewallRulesMock() ([]WinRule, error) {
+	jsonFile := "get_windows_firewall_rules_mock-v2.json"
+	fileContent, err := os.ReadFile(jsonFile)
+	if err != nil {
+		return nil, fmt.Errorf("error reading mock JSON file: %v", err)
+	}
+	var rawRules []WinRule
+	if err := json.Unmarshal(fileContent, &rawRules); err != nil {
+		return nil, fmt.Errorf("error parsing mock JSON file: %v", err)
+	}
+
+	// 轉換 LocalAddress
+	for i := range rawRules {
+		rawRules[i].LocalAddress = parseLocalAddress(rawRules[i].LocalAddress)
+	}
+
+	return rawRules, nil
+}
+
+func getLinuxFirewallRules() ([]Rule, error) {
+	cmd := exec.Command("sudo", "iptables-save")
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, fmt.Errorf("error getting Linux firewall rules: %v", err)
+	}
+
+	return parseIptablesRules(string(output)), nil
+}
+
+func parseIptablesRules(iptablesOutput string) []Rule {
+	var rules []Rule
+	var currentChain string
+	var ruleID int = 1
+
+	lines := strings.Split(iptablesOutput, "\n")
+	for _, line := range lines {
+		if strings.HasPrefix(line, "*") {
+			// Table definition
+			// table = line[1:]
+		} else if strings.HasPrefix(line, ":") {
+			// Chain definition
+			parts := strings.Fields(line[1:])
+			currentChain = parts[0]
+		} else if strings.HasPrefix(line, "-A") {
+			// Rule definition
+			parts := strings.Fields(line)
+			rule := Rule{
+				RuleID:          fmt.Sprintf("%d", ruleID),
+				RuleName:        fmt.Sprintf("%s-%d", currentChain, ruleID),
+				RuleDisplayName: fmt.Sprintf("%s-%d", currentChain, ruleID),
+				Chain:           currentChain,
+				Protocol:        "Any",
+				SrcAddress:      "Any",
+				DstAddress:      "Any",
+				SrcPort:         "*",
+				DstPort:         "*",
+				Action:          "allow",
+			}
+
+			for i := 2; i < len(parts); i++ {
+				switch parts[i] {
+				case "-p":
+					rule.Protocol = parts[i+1]
+					i++
+				case "-s":
+					rule.SrcAddress = parts[i+1]
+					i++
+				case "-d":
+					rule.DstAddress = parts[i+1]
+					i++
+				case "--sport":
+					rule.SrcPort = parts[i+1]
+					i++
+				case "--dport":
+					rule.DstPort = parts[i+1]
+					i++
+				case "-j":
+					rule.Action = strings.ToLower(parts[i+1])
+					if rule.Action == "accept" {
+						rule.Action = "allow"
+					} else if rule.Action == "drop" {
+						rule.Action = "deny"
+					}
+					i++
+				}
+			}
+
+			rules = append(rules, rule)
+			ruleID++
+		}
+	}
+
+	return rules
+}
+
+func createAlgoSecJSON(rules []Rule, osType string) AlgoSecData {
+	hostname, _ := os.Hostname()
+	systemInfo := map[string]string{
+		"name":          hostname,
+		"version":       runtime.GOOS,
+		"major_version": runtime.GOOS,
+		"minor_version": runtime.GOARCH,
+		"hostname":      hostname,
+	}
+
+	algosecData := AlgoSecData{
+		Version:        "1.3",
+		ConfigType:     "POLICY_BASED",
+		Device:         map[string]interface{}{"@type": "Device", "name": systemInfo["name"], "major_version": systemInfo["major_version"], "version": systemInfo["version"], "hostname": systemInfo["hostname"], "minor_version": systemInfo["minor_version"]},
+		Hosts:          make(map[string]interface{}),
+		HostsGroups:    make(map[string]interface{}),
+		Services:       make(map[string]interface{}),
+		ServicesGroups: make(map[string]interface{}),
+		Policies:       make(map[string]interface{}),
+	}
+
+	hosts := make(map[string]interface{})
+	services := make(map[string]interface{})
+
+	for _, rule := range rules {
+		srcAddresses := []string{rule.SrcAddress}
+		dstAddresses := []string{rule.DstAddress}
+		protocol := rule.Protocol
+		srcPort := rule.SrcPort
+		dstPort := rule.DstPort
+		action := rule.Action
+		direction := "inbound"
+		if rule.Chain == "OUTPUT" {
+			direction = "outbound"
+		}
+
+		// Add hosts
+		for _, addr := range srcAddresses {
+			if addr != "Any" {
+				hostName := fmt.Sprintf("ip_%s", strings.ReplaceAll(addr, "/", "_"))
+				hosts[hostName] = map[string]interface{}{
+					"name": hostName,
+					"ips":  []string{addr},
+					"type": "IP_ADDRESS",
+				}
+			}
+		}
+
+		for _, addr := range dstAddresses {
+			if addr != "Any" {
+				hostName := fmt.Sprintf("ip_%s", strings.ReplaceAll(addr, "/", "_"))
+				hosts[hostName] = map[string]interface{}{
+					"name": hostName,
+					"ips":  []string{addr},
+					"type": "IP_ADDRESS",
+				}
+			}
+		}
+
+		// Add services
+		serviceName := protocol
+		if srcPort != "*" {
+			serviceName += fmt.Sprintf("_%s", srcPort)
+		}
+		if dstPort != "*" {
+			serviceName += fmt.Sprintf("_to_%s", dstPort)
+		}
+
+		if _, exists := services[serviceName]; !exists {
+			serviceDef := map[string]interface{}{
+				"protocol": protocol,
+				"src_port": srcPort,
+				"dst_port": dstPort,
+			}
+			services[serviceName] = map[string]interface{}{
+				"name":                serviceName,
+				"service_definitions": []map[string]interface{}{serviceDef},
+				"type":                strings.ToUpper(protocol),
+			}
+		}
+
+		// Add policies
+		algosecRule := map[string]interface{}{
+			"rule_name":         rule.RuleID,
+			"rule_display_name": rule.RuleDisplayName,
+			"rule_id":           rule.RuleID,
+			"line_number":       rule.RuleID,
+			"rule_num":          rule.RuleID,
+			"src":               formatAddresses(srcAddresses),
+			"dst":               formatAddresses(dstAddresses),
+			"service":           []string{serviceName},
+			"action":            action,
+			"direction":         direction,
+			"comments":          fmt.Sprintf("Chain: %s | Table: %s", rule.Chain, rule.Table),
+			"log":               0,
+			"enable":            "enabled",
+		}
+
+		algosecData.Policies[rule.RuleID] = algosecRule
+	}
+
+	algosecData.Hosts = hosts
+	algosecData.Services = services
+
+	return algosecData
+}
+
+func formatAddresses(addresses []string) []string {
+	var formatted []string
+	for _, addr := range addresses {
+		if addr == "Any" {
+			return []string{"Any"}
+		}
+		formatted = append(formatted, fmt.Sprintf("ip_%s", strings.ReplaceAll(addr, "/", "_")))
+	}
+	return formatted
+}
+
+func main() {
+	osType := runtime.GOOS
+
+	var rules []Rule
+	var err error
+	if osType == "windows" {
+		fmt.Println("Detecting Windows system, getting firewall rules...")
+
+		getWindowsFirewallRulesMock()
+
+		//rules, err = getWindowsFirewallRules()
+	} else if osType == "linux" {
+		fmt.Println("Detecting Linux system, getting firewall rules...")
+		rules, err = getLinuxFirewallRules()
+	} else {
+		fmt.Printf("Unsupported operating system: %s\n", osType)
+		os.Exit(1)
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if len(rules) == 0 {
+		fmt.Println("No firewall rules found or error occurred.")
+		os.Exit(1)
+	}
+
+	algosecData := createAlgoSecJSON(rules, osType)
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println("Error getting hostname:", err)
+		os.Exit(1)
+	}
+	// ...existing code...
+	outputFile := fmt.Sprintf("%s_firewall_rules.algosec", hostname)
+	file, err := json.MarshalIndent(algosecData, "", "  ")
+	if err != nil {
+		fmt.Println("Error creating JSON:", err)
+		os.Exit(1)
+	}
+
+	if err := ioutil.WriteFile(outputFile, file, 0644); err != nil {
+		fmt.Println("Error writing to file:", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Successfully exported firewall rules to %s\n", outputFile)
+}
